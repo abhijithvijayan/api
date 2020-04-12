350b293e9d1492f48c62d99d5d83cc3e2e1f2df5
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-12 12:06:09 +0530

    fix: add scripts to use server with ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-/* eslint-disable no-console */
 import app from './core/app';
 import startJobs from './jobs';
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const startApp = () => {
+
+const startApp = (): void => {
     startJobs();
+
     try {
         app.set('port', PORT);
         app.listen(app.get('port'), () => {
             return console.log(`Server Running http://localhost:${PORT}`);
         });
-    } catch (error) {
-        console.error(`Error occured ${error}`);
+    } catch (err) {
+        console.error(`Error occured ${err}`);
     }
 };
 
